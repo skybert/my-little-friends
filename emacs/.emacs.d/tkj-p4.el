431917e9ef45830a46fbf8e546e1f30a09a94ee8
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2011-11-01 16:08:34 +0800

    - I use p4-diff-all-opened more than p4-diff2, so have re-defined   C-x p d

--- emacs/.emacs.d/tkj-p4.el
+++ emacs/.emacs.d/tkj-p4.el
@@ -7,3 +7,7 @@
 (setq p4-do-find-file nil
       p4-executable "/usr/local/bin/p4")
 (require 'vc-p4)
+
+;; I use p4-diff-all-opened more than p4-diff2
+(global-unset-key "\C-xpd")
+(global-set-key "\C-xpd" 'p4-diff-all-opened)
