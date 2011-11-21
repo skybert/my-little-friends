35703b912929133ed7c937c2723a46f9b2894fff
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2011-11-21 15:01:57 +0800

    - chat settings

---
+++ emacs/.emacs.d/tkj-chat.el
@@ -0,0 +1,37 @@
+(require 'erc-log)
+
+(setq erc-autojoin-channels-alist
+      '(("catbert" "#platform" "#innovation" "#developers")
+        ("irc.freenode.net" "#tossug"))
+      erc-log-channels-directory "~/.erc/logs/"
+      erc-log-write-after-send t
+      erc-save-buffer-on-part t
+      erc-modules (quote (autojoin
+                          button
+                          completion
+                          fill
+                          irccontrols
+                          keep-place
+                          list
+                          log
+                          match
+                          menu
+                          move-to-prompt
+                          netsplit
+                          networks
+                          noncommands
+                          readonly
+                          ring
+                          smiley
+                          stamp
+                          spelling
+                          track))
+
+      )
+;; save logs before quittign emacs
+;;(defadvice save-buffers-kill-emacs (before save-logs (arg) activate)
+;;  (save-some-buffers t (lambda ()
+;;                       (when (eq major-mode 'erc-mode) t))
+;;                         (when (and (eq major-mode 'erc-mode)
+;;                                    (not (null buffer-file-name))))
+;;                         ))
