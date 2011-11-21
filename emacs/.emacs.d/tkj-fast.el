86603f30e97d39e0aec01df48e2a3019ca317e03
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2011-11-21 15:34:12 +0800

    - added fast .emacs

---
+++ emacs/.emacs.d/tkj-fast.el
@@ -0,0 +1,36 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                                          ;; 
+;;           Torstein Krause Johansen's .emacs file,                        ;;
+;;           Minimum settings needed in any Emacs session.                  ;;
+;;                                                                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq user-full-name "Torstein Krause Johansen"
+      user-mail-address "tkj@vizrt.com"
+      mail-from-style 'angles)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Shortcuts in all modes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key "\C-xg" 'goto-line)
+(global-set-key "\M- " 'hippie-expand)
+(global-set-key "\C-\M-f" 'find-file-at-point)
+(global-set-key [ (control shift n) ] 'find-dired)
+(global-set-key  [ (control meta shift n) ] 'grep-find)
+;; don't write backslashed to indicate continuous lines
+(set-display-table-slot standard-display-table 'wrap ?\ )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Prefer UTF 8, but don't override current encoding if specified
+;; (unless you specify a write hook).
+;;
+;; This seems to solve the problem of Gnus encoding the message as
+;; utf-16be (64 encoded) when replying to something Bangladeshi.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(prefer-coding-system 'utf-8)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set tabs to 2 spaces and replace all tabs with spaces
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq default-tab-width 2)
+(setq-default indent-tabs-mode nil)
+
