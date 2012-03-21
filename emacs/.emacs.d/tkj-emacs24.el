5b81b811fc6f193ed7c97bfca91eb9395da2f0e5
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2012-03-21 10:32:00 +0800

    - added

---
+++ emacs/.emacs.d/tkj-emacs24.el
@@ -0,0 +1,9 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Themeing
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq custom-theme-load-path
+      '(custom-theme-directory t
+                               "/usr/local/src/djcb-elisp/themes" 
+                               "/mnt/debian/usr/local/src/djcb-elisp/themes")
+      load-cust)
+(load-theme 'zenburn t)
