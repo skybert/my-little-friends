f6c47d8a9865764360ccf51d87f8e72f007e05bd
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2012-03-21 10:12:40 +0800

    - commented out the compilation matching list as Emacs 24 seems to do   this right by itself.
    - added jtags

--- emacs/.emacs.d/tkj-java.el
+++ emacs/.emacs.d/tkj-java.el
@@ -4,17 +4,25 @@
 ;; Make Emacs understand Ant's and Maven's output
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'compile)
-(setq compilation-error-regexp-alist
-  (append (list
-           ;; works for jikes
-           '("^\\s-*\\[[^]]*\\]\\s-*\\(.+\\):\\([0-9]+\\):\\([0-9]+\\):[0-9]+:[0-9]+:" 1 2 3)
-           ;; works for javac
-           '("^\\s-*\\[[^]]*\\]\\s-*\\(.+\\):\\([0-9]+\\):" 1 2)
-           ;; works for maven 2.x
-           '("^\\(.*\\):\\[\\([0-9]*\\),\\([0-9]*\\)\\]" 1 2 3)
-           ;; works for maven 3.x
-           '("^\\(\\[ERROR\\] \\)?\\(/[^:]+\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\]" 2 3 4)
-           '("^\\(\\[WARNING\\] \\)?\\(/[^:]+\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\]" 2 3 4)
-           )
-          compilation-error-regexp-alist))
+
+;; (setq compilation-error-regexp-alist
+;;   (append (list
+;;           ;; works for jikes
+;;           '("^\\s-*\\[[^]]*\\]\\s-*\\(.+\\):\\([0-9]+\\):\\([0-9]+\\):[0-9]+:[0-9]+:" 1 2 3)
+;;           ;; works for javac
+;;           '("^\\s-*\\[[^]]*\\]\\s-*\\(.+\\):\\([0-9]+\\):" 1 2)
+;;           ;; works for maven 2.x
+;;           '("^\\(.*\\):\\[\\([0-9]*\\),\\([0-9]*\\)\\]" 1 2 3)
+;;           ;; works for maven 3.x
+;;           '("^\\(\\[ERROR\\] \\)?\\(/[^:]+\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\]" 2 3 4)
+;;           '("^\\(\\[WARNING\\] \\)?\\(/[^:]+\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\]" 2 3 4)
+;;           )
+;;          compilation-error-regexp-alist))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; jtags
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'load-path "/usr/local/src/jtags/src/lisp")
+(autoload 'jtags-mode "jtags" "Toggle jtags mode." t)
+(add-hook 'java-mode-hook 'jtags-mode)
 
