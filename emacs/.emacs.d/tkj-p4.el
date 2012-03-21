61138eeeff7eab637cd1176b2bdf96e1804a0e84
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2012-03-21 10:13:55 +0800

    - added my own, asynchronous p4 "mode", a number of methods   (overloading the standard p4 ones) that are a lot better when   working on slow networks (since they're not synchronous).

--- emacs/.emacs.d/tkj-p4.el
+++ emacs/.emacs.d/tkj-p4.el
@@ -6,8 +6,78 @@
 (require 'p4)
 (setq p4-do-find-file nil
       p4-executable "/usr/local/bin/p4")
-(require 'vc-p4)
+;; (require 'vc-p4)
 
 ;; I use p4-diff-all-opened more than p4-diff2
 (global-unset-key "\C-xpd")
 (global-set-key "\C-xpd" 'p4-diff-all-opened)
+
+;; my own, asynchronous p4 "mode".
+(setq tkj-p4-out-buffer "*tkj-p4-out*")
+
+(defun tkj-p4-edit()
+  (interactive)
+  (shell-command
+   (concat "p4 edit " (buffer-file-name))
+   (get-buffer-create tkj-p4-out-buffer)
+   (get-buffer-create tkj-p4-out-buffer))
+  (revert-buffer))
+
+(defun tkj-p4-revert()
+  (interactive)
+  (shell-command
+   (concat "p4 revert " (buffer-file-name) "&")
+   (get-buffer-create tkj-p4-out-buffer)
+   (get-buffer-create tkj-p4-out-buffer))
+  (revert-buffer))
+
+(defun tkj-get-diff-buffer()
+  (interactive)
+  (get-buffer-create "tkj-p4-diff-buffer"))
+
+(defun tkj-p4-diff()
+  (interactive)
+  (shell-command
+   (concat "p4 diff " (buffer-file-name) "&")
+   (tkj-get-diff-buffer)
+   (tkj-get-diff-buffer))
+;;  (save-excursion
+;;      (switch-to-buffer (tkj-get-diff-buffer))
+;;      (diff-mode))
+  )
+
+(defun tkj-p4-opened()
+  (interactive)
+  (shell-command
+   "p4 opened &"
+   (get-buffer-create tkj-p4-out-buffer)
+   (get-buffer-create tkj-p4-out-buffer)))
+
+(defun tkj-p4-filelog()
+  (interactive)
+  (shell-command
+   (concat "p4 filelog " (buffer-file-name) " &")
+   (get-buffer-create tkj-p4-out-buffer)
+   (get-buffer-create tkj-p4-out-buffer)))
+
+(defun tkj-p4-annotate()
+  (interactive)
+  (shell-command
+   (concat "p4 annotate -c " (buffer-file-name) " &")
+   (get-buffer-create tkj-p4-out-buffer)
+   (get-buffer-create tkj-p4-out-buffer)))
+
+(defun tkj-p4-submit()
+  (interactive)
+  (shell-command
+   (concat "xterm -e 'p4 submit " default-directory "...' &")
+   (get-buffer-create tkj-p4-out-buffer)
+   (get-buffer-create tkj-p4-out-buffer)))
+
+;; (global-set-key "\C-xpe" 'tkj-p4-edit)
+;; (global-set-key "\C-xpf" 'tkj-p4-filelog)
+;; (global-set-key "\C-xpr" 'tkj-p4-revert)
+(global-set-key "\C-xp=" 'tkj-p4-diff)
+;; (global-set-key "\C-xpo" 'tkj-p4-opened)
+(global-set-key "\C-xpS" 'tkj-p4-submit)
+;; (global-set-key "\C-xpV" 'tkj-p4-annotate)
