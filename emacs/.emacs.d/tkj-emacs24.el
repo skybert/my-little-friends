6dfd80ad2b74953dd2ccb73e4bc634f8675b4d09
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2012-03-27 15:35:18 +0800

    - added the solarized repo

--- emacs/.emacs.d/tkj-emacs24.el
+++ emacs/.emacs.d/tkj-emacs24.el
@@ -4,6 +4,7 @@
 (setq custom-theme-load-path
       '(custom-theme-directory t
                                "/usr/local/src/djcb-elisp/themes" 
+                               "/usr/local/src/solarized-emacs" 
                                "/mnt/debian/usr/local/src/djcb-elisp/themes")
       load-cust)
 (load-theme 'zenburn t)
