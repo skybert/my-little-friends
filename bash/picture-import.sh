1ea0b08580b79a9a6dfd8b538efcaff2cb101b2c
Torstein Krause Johansen <torstein@geronimo>
2012-05-12 21:19:00 +0200

    - added support for files and directories with spaces and other   strangeness in them.

--- bash/picture-import.sh
+++ bash/picture-import.sh
@@ -4,8 +4,8 @@
 
 src_dir=$HOME/put_the_new_pictures_in_here
 target_dir=/var/gallery
-error_dir=$HOME/picture-import/error
-log_file=$HOME/picture-import/log/$(basename $0 .sh).log
+error_dir=/usr/local/src/picture-import/picture-import/error
+log_file=/usr/local/src/picture-import/log/$(basename $0 .sh).log
 
 if [ $(which identify 2>/dev/null | wc -l) -eq 0 ]; then
   echo "You need imagemagick installed to use $(basename $0) :-("
@@ -18,13 +18,13 @@ function print_and_log() {
 }
 
 function create_dir_for_picture() {
-  if [ $(file $1 | cut -d':' -f2 | grep empty | wc -l) -gt 0 ]; then
+  if [ $(file "$@" | cut -d':' -f2 | grep empty | wc -l) -gt 0 ]; then
     print_and_log "The file" $1 "is empty :-( moving it to $error_dir"
     mv $1 $error_dir
     return 1
   fi
   
-  local date_dir=$(identify -format "%[EXIF:DateTimeOriginal]" $1 | \
+  local date_dir=$(identify -format "%[EXIF:DateTimeOriginal]" "$1" | \
     cut -d' ' -f1 | \
     sed 's#:#/#g')
 
@@ -35,10 +35,18 @@ function create_dir_for_picture() {
   fi
 
   print_and_log "Moving $1 -> $picture_dir ..."
-  mv $1 $picture_dir/
+  mv "$1" $picture_dir/
+  remove_picture_src_dir_if_empty "$1"
+  
 }
 
+function remove_picture_src_dir_if_empty() {
+  local dir=$(dirname "$1")
+  if [ $(find "$dir" -type f | wc -l) -eq 0 ]; then
+    rmdir "$dir"
+  fi
+}
 
 find $src_dir -iname "*.jpg" | while read f; do
-  create_dir_for_picture $f
+  create_dir_for_picture "$f"
 done
