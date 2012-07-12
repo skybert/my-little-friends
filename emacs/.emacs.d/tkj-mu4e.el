03a2254cbe094162c4944de33d71bb6d6df82349
Torstein Krause Johansen <torstein@geronimo>
2012-07-12 18:03:40 +0200

    - added SMTP configuration. Sending email now works using secrets from   authinfo.gpg

--- emacs/.emacs.d/tkj-mu4e.el
+++ emacs/.emacs.d/tkj-mu4e.el
@@ -1,3 +1,9 @@
 (setq mu4e-maildir "~/mail"
       mu4e-get-mail-command "offlineimap"
       )
+
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-stream-type 'starttls
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587)
