3ccf13341654f9beb9cc2c2b6d534cb6c70960bb
Torstein Krause Johansen <tkj@vizrt.com>
2012-07-25 09:45:24 +0200

    - don't save messages to Sent Messages, Gmail/IMAP will take care of this

--- emacs/.emacs.d/tkj-mu4e.el
+++ emacs/.emacs.d/tkj-mu4e.el
@@ -1,5 +1,9 @@
 (setq mu4e-maildir "~/mail"
       mu4e-get-mail-command "offlineimap"
+      
+      ;; don't save messages to Sent Messages, Gmail/IMAP will take
+      ;; care of this
+      mu4e-sent-messages-behavior 'trash
       )
 
 (setq message-send-mail-function 'smtpmail-send-it
