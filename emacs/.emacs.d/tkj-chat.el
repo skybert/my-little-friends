381d27898f8d9449bccdc072c78403deb1be99fc
Torstein Krause Johansen <tkj@vizrt.com>
2012-08-15 07:02:04 +0200

    - automatically invoking ERC for my IRC servers

--- emacs/.emacs.d/tkj-chat.el
+++ emacs/.emacs.d/tkj-chat.el
@@ -1,12 +1,6 @@
 (require 'erc-log)
 
-(setq erc-autojoin-mode t
-      erc-default-server "localhost"
-      erc-default-nicks "tkj"
-      erc-autojoin-channels-alist
-      '(("catbert.escenic.com" "#platform" "#innovation" "#developers")
-        ("localhost" "saas@conference.chat.ardendo.se")
-        ("irc.freenode.net" "#tossug"))
+(setq erc-default-server "localhost"
       erc-log-channels-directory "~/.erc/logs/"
       erc-log-write-after-send t
       erc-save-buffer-on-part t
@@ -32,7 +26,24 @@
                           track))
       )
 
-      
+(setq erc-autojoin-channels-alist
+      '(
+        ("catbert.escenic.com" "#developers" "#platform")
+        ("irc.freenode.net" "#emacs")
+        ))
+
+(erc :server "localhost" :port 6667 :nick "tkj")
+(erc :server "catbert.escenic.com" :port 6667 :nick "torstein")
+
+(defun my-reformat-jabber-backlog ()
+  "Fix \"unkown participant\" backlog messages from bitlbee."
+  (save-excursion
+    (goto-char (point-min))
+    (if (looking-at
+         "^<root> System message: Message from unknown participant \\([^:]+\\):")
+        (replace-match "<\\1>"))))
+(add-hook 'erc-insert-modify-hook 'my-reformat-jabber-backlog)
+
 ;; save logs before quittign emacs
 ;;(defadvice save-buffers-kill-emacs (before save-logs (arg) activate)
 ;;  (save-some-buffers t (lambda ()
