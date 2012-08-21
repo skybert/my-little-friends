d4164be24cbc855af14878f1c91f4538af444b7a
Torstein Krause Johansen <tkj@vizrt.com>
2012-08-21 08:55:54 +0200

    - re-worked the chat configuration

--- emacs/.emacs.d/tkj-chat.el
+++ emacs/.emacs.d/tkj-chat.el
@@ -1,37 +1,21 @@
+;; logging
 (require 'erc-log)
-
-(setq erc-default-server "localhost"
-      erc-log-channels-directory "~/.erc/logs/"
+(setq erc-log-channels-directory "~/.erc/logs/"
       erc-log-write-after-send t
-      erc-save-buffer-on-part t
-      erc-modules (quote (autojoin
-                          button
-                          completion
-                          fill
-                          irccontrols
-                          keep-place
-                          list
-                          log
-                          match
-                          menu
-                          move-to-prompt
-                          netsplit
-                          networks
-                          noncommands
-                          readonly
-                          ring
-                          smiley
-                          stamp
-                          spelling
-                          track))
-      )
+      erc-save-buffer-on-part t)
 
+;; auto join
+(require 'erc-join)
+(erc-autojoin-mode 1)
 (setq erc-autojoin-channels-alist
       '(
         ("catbert.escenic.com" "#developers" "#platform")
-        ("irc.freenode.net" "#emacs")
         ))
 
+;; general options
+(setq erc-default-server "localhost")
+
+;; connect to these IRC servers
 (erc :server "localhost" :port 6667 :nick "tkj")
 (erc :server "catbert.escenic.com" :port 6667 :nick "torstein")
 
@@ -44,10 +28,3 @@
         (replace-match "<\\1>"))))
 (add-hook 'erc-insert-modify-hook 'my-reformat-jabber-backlog)
 
-;; save logs before quittign emacs
-;;(defadvice save-buffers-kill-emacs (before save-logs (arg) activate)
-;;  (save-some-buffers t (lambda ()
-;;                       (when (eq major-mode 'erc-mode) t))
-;;                         (when (and (eq major-mode 'erc-mode)
-;;                                    (not (null buffer-file-name))))
-;;                         ))
