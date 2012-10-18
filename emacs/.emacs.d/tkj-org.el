e981de9b18076f84c96cab8877bf3907f455c15c
Torstein Krause Johansen <tkj@vizrt.com>
2012-10-18 10:03:54 +0200

    - added solartv to the agenda list

--- emacs/.emacs.d/tkj-org.el
+++ emacs/.emacs.d/tkj-org.el
@@ -3,6 +3,7 @@
  org-return-follows-link t 
  org-agenda-files (list "~/src/my-writings/work.org"
                         "~/src/my-writings/nie.org"
+                        "~/src/my-writings/solartv.org"
                         "~/src/my-writings/ece-scripts.org"
                         "~/src/nie/branches/5.5/doc/status.org"
                         "~/src/stv/trunk/doc/status.org"
