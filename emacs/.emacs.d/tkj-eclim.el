1e2671e3cfce527b66edcb2151a2fbfee500fd4b
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2012-12-04 08:26:13 +0100

    added my eclim configuration

---
+++ emacs/.emacs.d/tkj-eclim.el
@@ -0,0 +1,34 @@
+;; Add ELPA packages and emacs-eclim to the load path
+(let ((default-directory "~/.emacs.d/elpa"))
+  (normal-top-level-add-subdirs-to-load-path))
+(add-to-list 'load-path (expand-file-name "/usr/local/src/emacs/emacs-eclim"))
+
+;; Variables
+(setq eclim-auto-save t
+      eclim-executable "/opt/eclipse/eclim"
+      eclimd-executable "/opt/eclipse/eclimd"
+      eclimd-wait-for-process nil
+      eclimd-default-workspace "~/src"
+      help-at-pt-display-when-idle t
+      help-at-pt-timer-delay 0.1
+      )
+
+;; Load both eclim and eclimd (so that we can control eclimd from
+;; within Emacs)
+(require 'eclim)
+(require 'eclimd)
+
+;; Call the help framework with the settings above & activate
+;; eclim-mode
+(help-at-pt-set-timer)
+(global-eclim-mode)
+
+;; Hook eclim up with auto complete mode
+(require 'auto-complete-config)
+(ac-config-default)
+(require 'ac-emacs-eclim-source)
+(add-hook 'eclim-mode-hook (lambda ()
+                             (add-to-list 'ac-sources 'ac-source-emacs-eclim)
+                             (add-to-list 'ac-sources 'ac-source-emacs-eclim-c-dot)))
+
+
