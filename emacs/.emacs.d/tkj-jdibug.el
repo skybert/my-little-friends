b6fa2c3cc0d8d9643136edea5000914766232b54
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2013-01-01 19:44:04 +0100

    - trying to get it to work with emacs 24

--- emacs/.emacs.d/tkj-jdibug.el
+++ emacs/.emacs.d/tkj-jdibug.el
@@ -21,9 +21,23 @@
        )
 )
 
-(add-to-list 'load-path "/usr/local/src/jdibug")
+(add-to-list 'load-path "/usr/local/src/emacs/jdibug-0.5")
 ;; version 0.4 of jdibug requires semantic
-(require 'semantic)
+(setq semantic-load-turn-everything-on t)
+(semantic-mode 1)
+
+(require 'semantic/lex)
+(require 'semantic/senator)
+(require 'semantic/java)
+(require 'semantic/ia)
+(require 'semantic/ctxt)
+(require 'semantic/wisent)
+(require 'semantic/wisent/comp)
+(require 'semantic/wisent/java-tags)
+
+;; Use the full Java 1.5 grammer to parse Java
+(autoload 'wisent-java-default-setup "wisent" "Hook run to setup Semantic in 'java-mode'." nil nil)
+(require 'cl)
 (require 'jdibug)
 
 
