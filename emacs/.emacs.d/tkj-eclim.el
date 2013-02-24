6b78c9015e23834283043d2f61cae8d1bfff3150
Torstein Krause Johansen <tkj@conduct.no>
2013-02-24 15:11:47 +0100

    - updated emacs/eclim configuration to match the latest developments,   some of the AC configuration had changed, and - I also removed the   eclimd library as I wish to invoke it from the command line. - Lastly, I incrased the help timer delay.

--- emacs/.emacs.d/tkj-eclim.el
+++ emacs/.emacs.d/tkj-eclim.el
@@ -2,6 +2,9 @@
 (let ((default-directory "~/.emacs.d/elpa"))
   (normal-top-level-add-subdirs-to-load-path))
 
+(require 'eclim)
+(global-eclim-mode)
+
 ;; Variables
 (setq eclim-auto-save t
       eclim-executable "/opt/eclipse/eclim"
@@ -9,23 +12,15 @@
       eclimd-wait-for-process nil
       eclimd-default-workspace "~/src/workspace"
       help-at-pt-display-when-idle t
-      help-at-pt-timer-delay 0.1
+      help-at-pt-timer-delay 0.5
       )
-
-;; Load both eclim and eclimd (so that we can control eclimd from
-;; within Emacs)
-(require 'eclim)
-(require 'eclimd)
 
 ;; Call the help framework with the settings above & activate
 ;; eclim-mode
 (help-at-pt-set-timer)
-(global-eclim-mode)
 
 ;; Hook eclim up with auto complete mode
 (require 'auto-complete-config)
 (ac-config-default)
 (require 'ac-emacs-eclim-source)
-(add-hook 'eclim-mode-hook (lambda ()
-                             (add-to-list 'ac-sources 'ac-source-emacs-eclim)
-                             (add-to-list 'ac-sources 'ac-source-emacs-eclim-c-dot)))
+(ac-emacs-eclim-config)
