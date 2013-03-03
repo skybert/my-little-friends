72f85f12a70c4c57691c2290ae50a04bcb7c7020
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2013-03-03 16:56:30 +0100

    - updated java template to include the serialVersionUID to keep the   Eclipse compiler happy.

--- emacs/.emacs.d/auto-insert/insert.java
+++ emacs/.emacs.d/auto-insert/insert.java
@@ -5,13 +5,14 @@
  * @version 1.0
  */
 public final class (>>FILE_NO_EXT<<) {
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * No instances, please.
+     */
+    private (>>FILE_NO_EXT<<)() {
+    }
 
-  /**
-   * No instances, please.
-   */
-  private (>>FILE_NO_EXT<<)() {
-  }
-
-  public static void main(String args[]) {
-  }
+    public static void main(String args[]) {
+    }
 }
