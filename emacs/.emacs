b009aa548685a3210f258cc688d79f5edface65c
Torstein Krause Johansen <tkj@conduct.no>
2013-03-04 14:54:08 +0100

    - moved java settings to tkj-java - now loads tkj-java - updated fluxbox apps with firefox - increased eclim memory

--- emacs/.emacs
+++ emacs/.emacs
@@ -379,33 +379,9 @@
 (paren-activate)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; My c-mode settings, java specific settings are set in .emacs-java
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun my-c-mode-hook ()
-  (setq c-basic-offset 4
-        c-label-offset 0
-        indent-tabs-mode nil
-        compile-command "cd ~/src/DocEngine; mvn -o -DskipTests package"
-        require-final-newline nil)
-  (auto-fill-mode)
-  (c-set-offset 'substatement-open 0)
-  (define-key c-mode-base-map "\C-m" 'c-context-line-break)
-  (define-key c-mode-base-map "\C-c\C-i" 'yas/expand)
-  ;; Setting up a number of Java related shortcuts to mimic IDEA.
-  (define-key c-mode-base-map "\C-\M-g" 'eclim-java-find-declaration)
-  (define-key c-mode-base-map "\C-\M-o" 'eclim-java-import-organize)
-  (define-key c-mode-base-map "\C-q" 'eclim-java-show-documentation-for-current-element)
-  (define-key c-mode-base-map (kbd "<M-f7>") 'eclim-java-find-references)
-  (define-key c-mode-base-map (kbd "<S-f6>") 'eclim-java-refactor-rename-symbol-at-point)
-  
-  (subword-mode)
-  (gtags-mode)
-  )
-(add-hook 'c-mode-common-hook 'my-c-mode-hook)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Java
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(autoload 'java-mode "~/.emacs.d/tkj-java.el" t)
 (autoload 'java-mode "~/.emacs.d/tkj-eclim.el" t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- emacs/.emacs.d/tkj-java.el
+++ emacs/.emacs.d/tkj-java.el
@@ -1,21 +1,30 @@
 ;; -*- emacs-lisp -*-
+(defun tkj-insert-serial-version-uuid()
+  (interactive)
+  (insert "private static final long serialVersionUID = 1L;")
+)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Make Emacs understand Ant's and Maven's output
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'compile)
+(defun my-c-mode-hook ()
+  (setq c-basic-offset 4
+        c-label-offset 0
+        indent-tabs-mode nil
+        compile-command "cd ~/src/DocEngine; mvn -o -DskipTests package"
+        require-final-newline nil)
+  (auto-fill-mode)
+  (c-set-offset 'substatement-open 0)
+  (define-key c-mode-base-map "\C-m" 'c-context-line-break)
+  (define-key c-mode-base-map "\C-c\C-i" 'yas/expand)
+  ;; Setting up a number of Java related shortcuts to mimic IDEA.
+  (define-key c-mode-base-map "\C-\M-g" 'eclim-java-find-declaration)
+  (define-key c-mode-base-map "\C-\M-o" 'eclim-java-import-organize)
+  (define-key c-mode-base-map "\C-q" 'eclim-java-show-documentation-for-current-element)
+  (define-key c-mode-base-map (kbd "<M-f7>") 'eclim-java-find-references)
+  (define-key c-mode-base-map (kbd "<S-f6>") 'eclim-java-refactor-rename-symbol-at-point)
+  (define-key c-mode-base-map "\C-\M-j" 'tkj-insert-serial-version-uuid)
+  
+  (subword-mode)
+  (gtags-mode)
+  )
+(add-hook 'c-mode-common-hook 'my-c-mode-hook)
 
-;; (setq compilation-error-regexp-alist
-;;   (append (list
-;;           ;; works for jikes
-;;           '("^\\s-*\\[[^]]*\\]\\s-*\\(.+\\):\\([0-9]+\\):\\([0-9]+\\):[0-9]+:[0-9]+:" 1 2 3)
-;;           ;; works for javac
-;;           '("^\\s-*\\[[^]]*\\]\\s-*\\(.+\\):\\([0-9]+\\):" 1 2)
-;;           ;; works for maven 2.x
-;;           '("^\\(.*\\):\\[\\([0-9]*\\),\\([0-9]*\\)\\]" 1 2 3)
-;;           ;; works for maven 3.x
-;;           '("^\\(\\[ERROR\\] \\)?\\(/[^:]+\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\]" 2 3 4)
-;;           '("^\\(\\[WARNING\\] \\)?\\(/[^:]+\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\]" 2 3 4)
-;;           )
-;;          compilation-error-regexp-alist))
 
