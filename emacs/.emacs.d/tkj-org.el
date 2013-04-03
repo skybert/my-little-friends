445cb1565dee2873af33173b495529ce762616cf
Torstein Krause Johansen <tkj@conduct.no>
2013-04-03 15:18:17 +0200

    - added notes.org to agenda list

--- emacs/.emacs.d/tkj-org.el
+++ emacs/.emacs.d/tkj-org.el
@@ -12,6 +12,7 @@
  org-return-follows-link t
  org-agenda-files (list "~/doc/2013/conduct.org"
                         "~/doc/2013/broadnet.org"
+                        "~/doc/2013/notes.org"
                         )
  org-capture-templates(quote (("t" "todo" entry (file "~/doc/2013/notes.org")
                                "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)))
