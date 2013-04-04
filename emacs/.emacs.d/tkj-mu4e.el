553c606e04417de41c7c41bd15a20688f301c5b0
Torstein Krause Johansen <tkj@vizrt.com>
2013-04-04 22:21:29 +0200

    - expanded mu4e configuration to match several mail boxes

--- emacs/.emacs.d/tkj-mu4e.el
+++ emacs/.emacs.d/tkj-mu4e.el
@@ -10,21 +10,15 @@
       mu4e-view-show-images t
       mu4e-maildir-shortcuts
       '(
-        ("/gmailw/saas-alerts" . ?a)
-        ("/gmailw/community" . ?c)
-        ("/gmailw/developers-list" . ?d)
-        ("/gmailw/engine-5-list" . ?e)
-        ("/gmailw/vizrt-forum" . ?f)
-        ("/gmailw/inbox" . ?i)
-        ("/gmailw/jira" . ?j)
-        ("/gmailw/p4" . ?p)
-        ("/gmailw/saas" . ?s)
-        ("/gmailw/twitter" . ?t)
+        ("/conduct/inbox" . ?i)
+        ("/conduct/techtalk" . ?t)
+        ("/conduct/vizrt-forum" . ?v)
+        ("/conduct/wiki" . ?w)
         )
       )
 
 (setq message-send-mail-function 'smtpmail-send-it
-      user-mail-address "torsteinkrausework@gmail.com"
+      user-mail-address "tkj@conduct.no"
       smtpmail-stream-type 'starttls
       smtpmail-default-smtp-server "smtp.gmail.com"
       smtpmail-smtp-server "smtp.gmail.com"
@@ -33,16 +27,32 @@
 (defun tkj-load-mu4e-private()
   (interactive)
   (setq mu4e-maildir-shortcuts
-      '(
-        ("/gmail/community" . ?c)
-        ("/gmail/inbox" . ?i)
+        '(
+          ("/gmail/community" . ?c)
+          ("/gmail/inbox" . ?i)
+          )
+        user-mail-address "torstein.k.johansen@gmail.com"
         )
-      user-mail-address "torstein.k.johansen@gmail.com"
-      smtpmail-stream-type 'starttls
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587)
-)
+  )
+
+(defun tkj-load-mu4e-gmailw()
+  (interactive)
+  (setq mu4e-maildir-shortcuts
+        '(
+          ("/gmailw/saas-alerts" . ?a)
+          ("/gmailw/community" . ?c)
+          ("/gmailw/developers-list" . ?d)
+          ("/gmailw/engine-5-list" . ?e)
+          ("/gmailw/vizrt-forum" . ?f)
+          ("/gmailw/inbox" . ?i)
+          ("/gmailw/jira" . ?j)
+          ("/gmailw/p4" . ?p)
+          ("/gmailw/saas" . ?s)
+          ("/gmailw/twitter" . ?t)
+          )
+        user-mail-address "torsteinkrausework@gmail.com"
+        )
+  )
 
 ;; use imagemagick, if available
 (when (fboundp 'imagemagick-register-types)
