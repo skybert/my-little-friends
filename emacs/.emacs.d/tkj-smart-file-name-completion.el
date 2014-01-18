9e85b39f74f013fb8604e9e6c4fce2cb1eec56db
Torstein Krause Johansen <tkj@conduct.no>
2014-01-18 23:34:51 +0100

    - added configuration for setting up ido-ubiquitous (IDO everywhere)
    - added configuration for smex, enhanced M-x

--- emacs/.emacs.d/tkj-smart-file-name-completion.el
+++ emacs/.emacs.d/tkj-smart-file-name-completion.el
@@ -2,6 +2,8 @@
 ;; IDO & filecache: smart file name completion
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'ido)
+
+;; Improved flex matching
 (require 'flx-ido)
 
 (setq ido-everywhere nil
@@ -13,12 +15,20 @@
       )
 (ido-mode 'buffer)
 
+;; Vertical completion menu
 (require 'ido-vertical-mode)
 (ido-vertical-mode)
 
+;; IDO support pretty much everwhere, including eclim-java-implement
+(require 'ido-ubiquitous)
+(ido-ubiquitous)
+
+;; General project support
 (require 'projectile)
 (projectile-global-mode)
 (setq projectile-enable-caching nil)
-
 (global-set-key "\C-cf" 'projectile-find-file)
 
+;; Enhanced M-x
+(require 'smex)
+(global-set-key (kbd "M-x") 'smex)
