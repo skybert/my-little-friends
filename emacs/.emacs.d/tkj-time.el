a1730e978bf304b1b60ca747525b7a9239b6497d
Torstein Krause Johansen <tkj@conduct.no>
2014-07-04 09:17:56 +0200

    Added .el for time, calendar and date related settings.

---
+++ emacs/.emacs.d/tkj-time.el
@@ -0,0 +1,14 @@
+
+
+;; Display week numbers in the M-x calendar
+(copy-face font-lock-constant-face 'calendar-iso-week-face)
+(set-face-attribute 'calendar-iso-week-face nil
+                    :height 0.7)
+(setq calendar-intermonth-text
+      '(propertize
+        (format "%2d"
+                (car
+                 (calendar-iso-from-absolute
+                  (calendar-absolute-from-gregorian (list month day year)))))
+        'font-lock-face 'calendar-iso-week-face))
+
