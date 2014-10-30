8e13a6e8715baf4c738ff74302155cea49c3edf0
Torstein Krause Johansen <tkj@conduct.no>
2014-10-30 15:01:49 +0100

    Now determines the codepage automatically from the passed in text
    - if none is found (searches charset=, found in HTML/head), it falls
      back to utf-8

--- bash/tkj-html-to-text.sh
+++ bash/tkj-html-to-text.sh
@@ -1,8 +1,22 @@
 #! /usr/bin/env bash
 
 # by torstein.k.johansen@conduct.no
-
+default_code_page=utf-8
 tmp_file=$(mktemp)
+
+## $1 :: file
+function get_code_page_from_html() {
+    local result=$default_code_page
+    local cp=$(
+        cat $1 | sed -n 's#.*charset=\([^"]*\)".*#\1#p'
+    )
+
+    if [[ -n "$cp" ]]; then
+        result=$cp
+    fi
+
+    echo $result
+}
 
 function read_from_pipe() {
   if read -t 0; then
@@ -14,7 +28,7 @@ function read_from_pipe() {
 
 echo "$(read_from_pipe)" > $tmp_file
 if [ -x /usr/bin/links ]; then
-  links -dump $tmp_file -codepage utf-8
+  links -dump $tmp_file -codepage $(get_code_page_from_html $tmp_file)
 elif [ -x /usr/bin/lynx ]; then
   lynx -dump $tmp_file
 else
