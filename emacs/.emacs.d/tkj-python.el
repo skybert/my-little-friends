29e502d43128e756f24ee9457035d989f722b1fe
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2015-03-16 21:44:22 +0100

    Added Python specific configuration

---
+++ emacs/.emacs.d/tkj-python.el
@@ -0,0 +1,14 @@
+;; Python setup
+
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'ac-anaconda-setup)
+
+(setq python-shell-virtualenv-path "/home/torstein/src/atelier/src/main/python/pyenv")
+
+(pyenv-mode)
+(defun projectile-pyenv-mode-set ()
+  "Set pyenv version matching project name.
+Version must be already installed."
+  (pyenv-mode-set (projectile-project-name)))
+
+(add-hook 'projectile-switch-project-hook 'projectile-pyenv-mode-set)
