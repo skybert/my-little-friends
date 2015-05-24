8647dd642e5b752ad3447bdf82173946c98cd31e
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2015-05-24 18:54:39 +0200

    Removed (other/old) version of tkj-chat
    - don't use it anymore

--- emacs/.emacs.d/tkj-chat.el
+++ emacs/.emacs.d/tkj-chat.el
@@ -17,11 +17,6 @@
 ;; general options
 (setq erc-default-server "localhost")
 
-;; connect to these IRC servers
-(defun tkj-load-chat()
-  (interactive)
-  (erc :server "localhost" :port 6667 :nick "torstein"))
-
 ;; Spell checking
 (add-hook 'erc-mode-hook 'flyspell-mode)
 
