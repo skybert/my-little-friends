4c6c5ae41a86c706f5b701b62ecf845954059270
Torstein Krause Johansen <torstein@escenic.com>
2015-09-07 17:36:33 +0200

    Updated insert header to standard escenic header

--- emacs/.emacs.d/auto-insert/insert.java
+++ emacs/.emacs.d/auto-insert/insert.java
@@ -1,9 +1,19 @@
-package net.skybert;
+/**
+ * $Header$
+ *
+ * Copyright (C) 2015 Escenic AS.
+ *
+ * All Rights Reserved.  No use, copying or distribution of this work
+ * may be made except in accordance with a valid license agreement
+ * from Escenic AS.  This notice must be included on all copies,
+ * modifications and derivatives of this work.
+ */
+package com.escenic;
 
 /**
  * (>>FILE_NO_EXT<<)
  *
- * @author Torstein Krause Johansen
+ * @author <a href="mailto:torstein@escenic.com">Torstein Krause Johansen</a>
  * @version $Revision$ $Date$
  */
 public class (>>FILE_NO_EXT<<) {
