7f523eb038f894abf8d383ac0c00e4e9c7be3068
Torstein Krause Johansen <torstein@escenic.com>
2015-12-02 09:08:13 +0100

    Smarter occur: occur-dwim

--- emacs/.emacs.d/tkj-smart-file-name-completion.el
+++ emacs/.emacs.d/tkj-smart-file-name-completion.el
@@ -37,6 +37,22 @@
       )
 (global-set-key (kbd "C-;") 'projectile-find-file)
 
+;; Show search hits of strings in current buffer
+;; http://oremacs.com/2015/01/26/occur-dwim/
+(defun occur-dwim ()
+  "Call `occur' with a sane default."
+  (interactive)
+  (push (if (region-active-p)
+            (buffer-substring-no-properties
+             (region-beginning)
+             (region-end))
+          (let ((sym (thing-at-point 'symbol)))
+            (when (stringp sym)
+              (regexp-quote sym))))
+        regexp-history)
+  (call-interactively 'occur))
+(global-set-key (kbd "M-s o") 'occur-dwim)
+
 ;; General springboard to switch between often used directories/
 ;; projects.
 (require 'springboard)
