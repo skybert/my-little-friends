9bda8d4d58b3ab9dc0fbf4859c08d2798e6687db
Torstein Krause Johansen <torstein@escenic.com>
2016-04-13 11:00:19 +0200

    Added world clock conf

--- emacs/.emacs.d/tkj-time.el
+++ emacs/.emacs.d/tkj-time.el
@@ -2,9 +2,9 @@
 ;; Insert date
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun insert-date ()
-   (interactive)
-   (let (( time (current-time-string) ))
-     (insert (format-time-string "%Y-%m-%d"))))
+  (interactive)
+  (let (( time (current-time-string) ))
+    (insert (format-time-string "%Y-%m-%d"))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -21,3 +21,11 @@
                   (calendar-absolute-from-gregorian (list month day year)))))
         'font-lock-face 'calendar-iso-week-face))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; world clock
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq display-time-world-list
+      '(("America/Argentina/Buenos_Aires" "Buenos Aires")
+        ("Europe/Oslo" "Oslo")
+        ("Asia/Dhaka" "Dhaka")
+        ("Asia/Taipei" "Taipei")))
