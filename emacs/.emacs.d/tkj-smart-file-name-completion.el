960d381d15f2fc3cac04b8b1c0e185d75604fc64
Torstein Krause Johansen <torstein@escenic.com>
2016-06-08 14:56:04 +0200

    Append, not overwrite the nice ignore directories defaults from projectile

--- emacs/.emacs.d/tkj-smart-file-name-completion.el
+++ emacs/.emacs.d/tkj-smart-file-name-completion.el
@@ -32,7 +32,7 @@
 (require 'projectile)
 (projectile-global-mode)
 (setq projectile-enable-caching nil
-      projectile-globally-ignored-directories '("target")
+      projectile-globally-ignored-directories (append (list "target") projectile-globally-ignored-directories)
       projectile-globally-ignored-file-suffixes '("iml" "ipr" "classpath")
       projectile-tags-command "/usr/bin/ctags -Re -f \"%s\" %s"
       )
