75957bc35edf4e3919d39559d81c738f143c5b96
Torstein Krause Johansen <torstein@escenic.com>
2016-07-18 15:20:48 +0200

    A really simple method to tidy up the worst ugliness of minified CSS

--- emacs/.emacs.d/tkj-web.el
+++ emacs/.emacs.d/tkj-web.el
@@ -9,3 +9,19 @@
           '(lambda ()
              (yas-minor-mode)
              ))
+
+(defun tkj-tidy-up-css()
+  (interactive)
+  (goto-char 0)
+  (replace-string ";" ";
+")
+  (goto-char 0)
+  (replace-string "{" "
+{
+")
+  (goto-char 0)
+  (replace-string "}" "
+}
+")
+  (indent-region (point-min) (point-max)))
+(global-set-key (kbd "C-x s") 'tkj-tidy-up-css)
