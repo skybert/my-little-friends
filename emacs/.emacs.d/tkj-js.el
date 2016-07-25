abe8611df88c29068d22f4973cf421eb155ab663
Torstein Krause Johansen <torstein@escenic.com>
2016-07-25 20:29:57 +0200

    Don't load ts mode by default

--- emacs/.emacs.d/tkj-js.el
+++ emacs/.emacs.d/tkj-js.el
@@ -14,29 +14,31 @@
 
 (defadvice js2-parse-statement (around json)
   (if (and (= tt js2-LC)
-      js2-buffer-file-name
-      (string-equal (substring js2-buffer-file-name -5) ".json")
-      (eq (+ (save-excursion
-            (goto-char (point-min))
-            (back-to-indentation)
-            (while (eolp)
-              (next-line)
-              (back-to-indentation))
-            (point)) 1) js2-ts-cursor))
-    (setq ad-return-value (js2-parse-assign-expr))
+           js2-buffer-file-name
+           (string-equal (substring js2-buffer-file-name -5) ".json")
+           (eq (+ (save-excursion
+                    (goto-char (point-min))
+                    (back-to-indentation)
+                    (while (eolp)
+                      (next-line)
+                      (back-to-indentation))
+                    (point)) 1) js2-ts-cursor))
+      (setq ad-return-value (js2-parse-assign-expr))
     ad-do-it))
 (ad-activate 'js2-parse-statement)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Typescript mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'typescript)
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
-(require 'tss)
+(defun tkj-load-typescript()
+  (interactive)
+  (require 'typescript)
+  (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+  (require 'tss)
 
-(setq tss-popup-help-key "C-:"
-      tss-jump-to-definition-key "C->"
-      tss-implement-definition-key "C-c j"
-      typescript-indent-level 2)
+  (setq tss-popup-help-key "C-:"
+        tss-jump-to-definition-key "C->"
+        tss-implement-definition-key "C-c j"
+        typescript-indent-level 2)
 
-(tss-config-default)
+  (tss-config-default))
