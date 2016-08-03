1d8e44d56efdbf3c9137ab98f6499b0ded1346d4
Torstein Krause Johansen <torstein@escenic.com>
2016-08-03 12:22:35 +0200

    Not using AC anymore, so no ac-emoji-setup in vc mode

--- emacs/.emacs.d/tkj-vc.el
+++ emacs/.emacs.d/tkj-vc.el
@@ -1,7 +1,5 @@
 ;; VC related changes, also see tkj-p4.el for p4 specific changes
 
-(add-hook 'git-commit-mode-hook 'ac-emoji-setup)
-
 ;; Remove the 'Git-' prefix from the modeline when displaying the
 ;; current branch.
 (setcdr (assq 'vc-mode mode-line-format)
