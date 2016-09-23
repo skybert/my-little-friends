28696f75078d7ce752386322da33f9028ad7d45f
Torstein Krause Johansen <torstein@escenic.com>
2016-09-23 15:13:27 +0200

    Now includes meetings too

--- bash/org-agenda.sh
+++ bash/org-agenda.sh
@@ -10,7 +10,7 @@ set -o pipefail
 shopt -s nullglob
 
 main() {
-  format=${1-""}
+  format=${1-"markdown"}
 
   now=
   start_date=
@@ -24,19 +24,26 @@ main() {
           org-agenda-start-day \"${start_date}\"
           org-agenda-span 8
           org-agenda-include-diary t
-          org-agenda-files (quote (\"~/doc/work.org\")))" \
+          org-agenda-files (quote (\"~/doc/work.org\" \"~/doc/gcal.org\")))" \
             2>/dev/null)
 
   result=
   result=$(
     echo "${org_agenda}" |
+      egrep -v '^[ ]+[0-9][0-9]?' |
+      sed -r 's#gcal.*Scheduled:#Meeting:#' |
+      sed -r 's#gcal:.*DONE#Meeting:#' |
+      grep -v 'Onelinescrum' |
+      egrep -v '^Diary:' |
       sed -r -e 's#:([^:^ ]+)#\#\1 #g' -e 's# :$##' |
       sed -r 's#work:.* Sched. [0-9]+x:.*STARTED#  CONTINUE#' |
-      sed -r 's#work:.* Sched.*:##' |
+      sed -r 's#work:.* Scheduled:##' |
+      sed -r 's#work:.* Sched. [0-9]*x:##' |
       sed -r 's#TODO ##' |
       sed -r 's#PR #⌛ Fixed, awaiting PR: #' |
       sed -r 's#DONE #✔ #' |
-      sed -r 's#MERGED #✔ Merged: #')
+      sed -r 's#MERGED #✔ Merged: #'
+        )
 
   if [[ ${format} == "markdown" ]]; then
     echo "${result}" | \
