a00381ef0a12e96b68064e7c1fb8af3e78e0569b
Torstein Krause Johansen <torstein@escenic.com>
2017-02-17 08:28:22 +0100

    Added methods to turn on and off all shunit tests

--- emacs/.emacs.d/tkj-sh.el
+++ emacs/.emacs.d/tkj-sh.el
@@ -6,3 +6,17 @@
 
 ;; on the fly syntax checking
 (add-hook 'sh-mode-hook 'flycheck-mode)
+
+(defun tkj-shunit-disable-all-tests()
+  (interactive)
+  (save-excursion
+    (goto-char 0)
+    (while (search-forward "test_" nil t)
+      (replace-match "not_test_" nil t))))
+
+(defun tkj-shunit-enable-all-tests()
+  (interactive)
+  (save-excursion
+    (goto-char 0)
+    (while (search-forward "not_test_" nil t)
+      (replace-match "test_" nil t))))
