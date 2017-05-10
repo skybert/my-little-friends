f29c520fb978c605e040bcc4d1aba7423c0a1f68
Torstein Krause Johansen <torstein@escenic.com>
2017-05-10 09:46:46 +0200

    Removed dead code

--- emacs/.emacs.d/tkj-org.el
+++ emacs/.emacs.d/tkj-org.el
@@ -21,12 +21,12 @@
                              "~/doc/wunderlist.org"
                              )
       org-capture-templates
-      (quote (("t" "todo" entry (file "~/doc/work.org")
+      (quote (("t" "todo" entry
+               (file
+                (concat "~/doc/scribbles/" (format-time-string "%Y") "/work.org"))
                "** TODO %?\n  SCHEDULED: %T\n%a\n")
               ("i" "idea" entry (file "~/doc/ideas.org")
-               "** TODO %?\n  SCHEDULED: %T\n%a\n")
-              ("q" "QA follow up" entry (file "~/doc/work.org")
-               "** TODO Follow up on QA of %?\n  SCHEDULED: %T\n%a\n"))))
+               "** TODO %?\n  SCHEDULED: %T\n%a\n"))))
 
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cl" 'org-store-link)
@@ -38,57 +38,4 @@
 
 ;; restore windows after showing the agenda
 ;; (add-hook 'org-agenda-after-show-hook 'winner-undo)
-
-;; don't create a footer
-(setq
- org-export-html-style "<link rel=\"stylesheet\" type=\"text/css\" href=\"conduct.css\">"
- org-export-html-postamble nil
-)
-
-(defun tkj-export-to-confluence()
-  (interactive)
-  ;; TODO create new file with <filename>.confluence in the same
-  ;; directory as the .org file
-  (save-excursion
-    (write-file (concat (buffer-name) ".confluence"))
-    (goto-line 1)
-    (query-replace "#+BEGIN_SRC js2" "{code}")
-    (goto-line 1)
-    (query-replace "#+BEGIN_SRC html" "{code}")
-    (goto-line 1)
-    (query-replace "#+BEGIN_SRC conf" "{code}")
-    (goto-line 1)
-    (query-replace "#+BEGIN_SRC java" "{code}")
-    (goto-line 1)
-    (query-replace "#+BEGIN_SRC sh" "{code}")
-    (goto-line 1)
-    (query-replace "#+BEGIN_SRC nxml" "{code}")
-    (goto-line 1)
-    (query-replace "#+BEGIN_SRC text" "{code}")
-    (goto-line 1)
-    (query-replace "#+END_SRC" "{code}")
-    (goto-line 1)
-    (query-replace "#+BEGIN_SRC" "{code}")
-    (goto-line 1)
-    (query-replace "****" "h4.")
-    (goto-line 1)
-    (query-replace "***" "h3.")
-    (goto-line 1)
-    (query-replace "**" "h2.")
-    (goto-line 1)
-    (query-replace "*" "h1.")
-    (goto-line 1)
-    (query-replace "
-- " "
-* ")
-    (goto-line 1)
-    (query-replace "
-
-| " "
-|| ")
-    (goto-line 1)
-    (query-replace "|
-|-" "||
-|-"))
-  )
 
