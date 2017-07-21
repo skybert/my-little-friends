4e1e475e72c53e06817ac50e2447d77fcb890b1d
Torstein Krause Johansen <torstein@escenic.com>
2017-07-21 10:18:03 +0800

    Removed dead code

--- emacs/.emacs.d/tkj-org.el
+++ emacs/.emacs.d/tkj-org.el
@@ -5,8 +5,6 @@
 (require 'org-element)
 (require 'org)
 
-;; (require 'ox-reveal)
-
 (setq org-return-follows-link t
       org-reveal-theme "blood" ;; serif
       org-reveal-root "http://skybert.net/reveal.js"
@@ -27,7 +25,3 @@
 (setq remember-annotation-functions '(org-remember-annotation))
 (setq remember-handler-functions '(org-remember-handler))
 (add-hook 'remember-mode-hook 'org-remember-apply-template)
-
-;; restore windows after showing the agenda
-;; (add-hook 'org-agenda-after-show-hook 'winner-undo)
-
